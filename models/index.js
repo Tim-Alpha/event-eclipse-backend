f559b6352c30eceb9b238e3ea2d46e5415f1cd7e
Tim-Alpha <codewithaisha@gmail.com>
2024-04-17 11:14:06 +0530

    refactor: code and implement code from basic

--- models/index.js
+++ models/index.js
@@ -1,47 +1,43 @@
-import fs from 'fs';
-import path from 'path';
-import { Sequelize, DataTypes } from 'sequelize';
-import dotenv from 'dotenv'
+'use strict';
 
-dotenv.config()
-import config from '../config/config.json' assert { type: "json" };
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-console.log(config['development'].host);
-
-const sequelize = new Sequelize({
-  dialect: 'mariadb',
-  host: config['development'].host,
-  database: config['development'].database,
-  username: config['development'].username,
-  password: config['development'].password,
-});
-
-const dbModels = {};
-
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
-      file !== 'index.js' && // Exclude index.js
-      file.slice(-3) === '.js'
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file)).default;
-    dbModels[model.name] = model.init(sequelize, DataTypes);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
   });
 
-Object.values(dbModels).forEach(model => {
-  if (model.associate) {
-    model.associate(dbModels);
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
 });
 
-const db = {
-  sequelize: sequelize,
-  models: dbModels,
-};
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
